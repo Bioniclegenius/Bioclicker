b3bb3b573661c45e7c9f28206445279059806d3b
David Berry <Bioniclegenius@users.noreply.github.com>
2019-05-12 00:34:53 -0500

    Basic window setup

--- src/Bioclicker/Game.java
+++ src/Bioclicker/Game.java
@@ -1,9 +1,22 @@
 package Bioclicker;
 
-import java.util.List;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Game {
     public Resources resPool;
+    public JFrame frame;
+    
+    public Game(){
+        frame = new JFrame("Bioclicker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(new Color(0, 0, 0));
+        frame.pack();
+        frame.setMinimumSize(new Dimension(400, 400));
+        frame.setVisible(true);
+    }
     
     public void Run(){
 	    
