77a6d9720aaad7a3aa9ce3d05784f99dd6c0f414
Robin de Zwart <robindezwart11@gmail.com>
2019-05-12 18:56:24 -0700

    Tidied up the JFrame/JPanel situation.

--- src/Bioclicker/Game.java
+++ src/Bioclicker/Game.java
@@ -1,24 +1,37 @@
 package Bioclicker;
 
-import java.awt.Color;
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.JFrame;
-
-public class Game {
+public class Game extends JPanel {
     public Resources resPool;
     public JFrame frame;
-    
-    public Game(){
-        frame = new JFrame("Bioclicker");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(new Color(0, 0, 0));
-        frame.pack();
-        frame.setMinimumSize(new Dimension(400, 400));
-        frame.setVisible(true);
+
+    public Game() {
+        setPreferredSize(new Dimension(400, 400));
+
+        long curTime = System.nanoTime();
+        int milliPerTick = 100;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            if (System.nanoTime() - curTime >= 1000000 * milliPerTick) {
+                run();
+                curTime += 1000000 * milliPerTick;
+            }
+        }
     }
-    
-    public void Run(){
-	    
+
+    public void run() {
+        System.out.println("running");
+        repaint();
     }
+
+    // Method for actually drawing to the screen.
+    // If this runs, the code is working.
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        System.out.println("painting");
+        setBackground(Color.BLUE);
+    }
+
+
 }
--- src/Bioclicker/Main.java
+++ src/Bioclicker/Main.java
@@ -1,16 +1,25 @@
 package Bioclicker;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public Main(String title) {
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Game game = new Game();
+        this.add(game);
+
+        this.pack();
+        this.setMinimumSize(new Dimension(400, 400));
+        this.pack();
+        this.setVisible(true);
+    }
 
     public static void main(String[] args) {
-        Game game = new Game();
-        long curTime = System.nanoTime();
-        int milliPerTick = 100;
-        while(true){
-            if(System.nanoTime() - curTime >= 1000000 * milliPerTick){
-                game.Run();
-                curTime += 1000000 * milliPerTick;
-            }
-        }
+        new Main("Bioclicker");
     }
 }
